b2d3bff47a6d4f68e2c9f4bed8f0531ab8f91c42
Denis Lykov <cailean393@gmail.com>
2020-01-03 17:28:16 +0300

    add validation to login form

--- src/components/loginForm/loginForm.tsx
+++ src/components/loginForm/loginForm.tsx
@@ -1,27 +1,45 @@
 import React from 'react'
-import { FaUser, FaUnlockAlt } from 'react-icons/fa'
+import { FaUser, FaUnlockAlt, FaSpinner } from 'react-icons/fa'
 import { Link } from '@reach/router'
+import { Field, ErrorMessage, Form, withFormik } from 'formik'
+import * as Yup from 'yup'
 import s from './loginForm.module.scss'
 
-const LoginForm = () => {
+const LoginForm = ({ isValid, isSubmitting, errors, touched }: any) => {
   return (
     <div className={s.formPanel}>
       <h2 className={s.formLabel}>Authorization</h2>
-      <form autoComplete='off'>
+      <Form autoComplete='off'>
         <div className={s.formField}>
           <FaUser className={s.formIcon} />
-          <input className={s.input} type="text" placeholder="Name" name="name" />
+          <Field 
+            className={s.input} 
+            type="text" 
+            placeholder="Name" 
+            name="name" 
+            error={touched.name && errors.name}
+          />
           <div className={s.inputLine} data-line />
+          <ErrorMessage className={s.errorMessage} name="name" component="div"/>
         </div>
         <div className={s.formField}>
           <FaUnlockAlt className={s.formIcon} />
-          <input className={s.input} type="password" placeholder="Password" name="password" />
+          <Field 
+            className={s.input} 
+            type="password" 
+            placeholder="Password" 
+            name="password" 
+            error={touched.password && errors.password}
+          />
           <div className={s.inputLine} data-line />
+          <ErrorMessage className={s.errorMessage} name="password" component="div"/>
         </div>
         <div className={s.submitPanel}>
-          <button className={s.submitButton} type='submit'>Sign in</button>
+          <button className={s.submitButton} type='submit' disabled={!isValid || isSubmitting}>
+            {isSubmitting ? <FaSpinner className="icon-spin"/> : 'Sign in'}
+          </button>
         </div>
-      </form>
+      </Form>
       <div className={s.regPanel}>
         <span>Do not you have an account?</span>
         <Link className={s.regLink} to="/reg">Registration</Link>
@@ -30,4 +48,27 @@ const LoginForm = () => {
   )
 }
 
-export default LoginForm
+
+const validationSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(3, 'Too Short!')
+    .max(30, 'Too Long!')
+    .matches(/^[A-Za-zА-Яа-я\s-_]+[0-9]*$/, 'Invalid characters')
+    .required('Required'),
+  password: Yup.string()
+    .min(6, 'Too Short!')
+    .max(30, 'Too Long!')
+    .required('Required'),
+});
+
+const FormikForm = withFormik<any, any>({
+  displayName: 'LoginForm',
+  mapPropsToValues: () => ({ name: '', password: '' }),
+  validationSchema: validationSchema,
+  validateOnMount: true,
+  handleSubmit(values, { setSubmitting, props }) {
+
+  }
+})(LoginForm)
+
+export default FormikForm
